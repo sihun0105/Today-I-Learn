6f9654bcf446c6e2c145a3a501066b03005dc0ad
sihun <thrchtlgns@naver.com>
2022-12-15 19:28:52 +0900

    update

---
+++ algorithm/LV1/약수의합.js
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12928
+function solution(n) {
+  var answer = 0;
+  for (let i = 0; i <= n; i++) {
+    if (n % i === 0) answer += i;
+  }
+  return answer;
+}
